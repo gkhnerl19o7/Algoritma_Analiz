4f925a4a5452b06fa649027996e4634970c13a44
Gökhan <gokhnerl19o7@outlook.com>
2025-03-02 13:35:48 +0300

    Hafta 3 big O
    Algoritma analizi 3.hafta ödev

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+class Program
+{
+   static int IkiliArama(int arananSayi, int[] sayilar)
+    {
+
+        int baslangic = 0, bitis = sayilar.Length, orta;//baslangic indisi,bitis indisi ve ortanca indis belirlenir.
+        int adimsayisi = 1;
+        while (bitis - baslangic > 1)  //dizide tek eleman kalınca döngü biter.
+        {
+            orta = (baslangic + bitis) / 2;
+            
+            if (sayilar[orta] > arananSayi)//ortanca sayı aranan sayıdan büyük ise veri kümesini küçük tarafın sınırlarıyla daralt
+            {
+                bitis = orta;
+                adimsayisi++;
+            }
+            else if (sayilar[orta] < arananSayi)//ortanca sayı aranan sayıdan küçük ise veri kümesini büyük tarafın sınırlarıyla daralt
+            {
+                baslangic = orta;
+                adimsayisi += 1;
+            }
+            else
+            {
+                return adimsayisi;
+            }
+        }
+        return -1;
+
+    }
+
+    static void Main()
+    {
+        int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine(IkiliArama(8, ints));
+        
+    }
+}
+
+
---
+++ Recursive_uslusayi/Recursive_uslusayi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 2;
+
+        int b = 4;
+
+        long sonuc = UsAl(a, b);
+        Console.WriteLine($"{a}^{b} = {sonuc}");
+    }
+
+    static long UsAl(int a, int b)
+    {
+        Console.WriteLine(a);
+        if (b == 0) // a^0 = 1
+            return 1;
+        if (b == 1) // a^1 = a
+            return a;
+        if(b%2 == 0)
+        {
+            a = a * a;
+            // özyineleme
+            return UsAl(a, b / 2);
+        }
+        
+        return a * UsAl(a, b - 1); // özyineleme
+    }
+}
