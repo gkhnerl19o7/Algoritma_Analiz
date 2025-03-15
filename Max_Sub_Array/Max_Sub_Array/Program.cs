3635aa176b09b8f870c89ae6f04bc838ff403422
Gökhan <gokhnerl19o7@outlook.com>
2025-03-15 13:51:55 +0300

    Hafta4
    max_sub_array algoritması

---
+++ Max_Sub_Array/Max_Sub_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static int MaxSubArray(int[] arr)
+    {
+        int maxSum = int.MinValue;
+        int currentSum = 0;
+
+        foreach (int num in arr)
+        {
+            currentSum = Math.Max(num, currentSum + num);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    static void Main()
+    {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        Console.WriteLine("Maksimum Alt Dizi Toplamı: " + MaxSubArray(arr));
+        // Çıktı: 6 (Alt dizi: [4, -1, 2, 1])
+    }
+}
